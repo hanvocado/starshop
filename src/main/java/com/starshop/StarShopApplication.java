59bae8a9fc25aacf3b85bc43ef540afe26833311
ntnhtc <han.ntnhtc@gmail.com>
2024-10-23 14:34:23 +0700

    init project

---
+++ src/main/java/com/starshop/StarShopApplication.java
@@ -0,0 +1,13 @@
+package com.starshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarShopApplication.class, args);
+	}
+
+}
